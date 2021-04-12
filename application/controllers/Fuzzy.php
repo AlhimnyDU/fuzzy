0e2a687d592caaad64d0ca3a2639cfbf8d8d8d51
AlhimnyDU <dwinata1@mhs.itenas.ac.id>
2021-04-13 05:11:57 +0700

    heroku

--- application/controllers/Fuzzy.php
+++ application/controllers/Fuzzy.php
@@ -22,7 +22,6 @@ class Fuzzy extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->database();
         $this->load->library('session');
         $this->load->library('upload');
         $this->load->library('pagination');
